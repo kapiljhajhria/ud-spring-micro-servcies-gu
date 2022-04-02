d07b285ab95a2b826e1ddf8957b29dc317a73939
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-02 19:24:29 +0530

    Spring MVC init

---
+++ brewery/src/main/java/jhajhria/com/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package jhajhria.com.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryApplication.class, args);
+	}
+
+}
