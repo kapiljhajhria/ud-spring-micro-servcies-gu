96d7f9e7b0ee204c895ec39caad1862e7f83a4fe
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-03 13:46:10 +0530

    BeerController Methods created - min implementation only

---
+++ mssc-beer-service/src/main/java/com/jhajhria/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,43 @@
+package com.jhajhria.msscbeerservice.web.controller;
+
+import com.jhajhria.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //todo impl
+        return ResponseEntity.ok(BeerDto.builder().id(beerId).beerName("Beer Name").build());
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+        BeerDto createdBeerDto = BeerDto.builder().id(UUID.randomUUID()).beerName(beerDto.getBeerName()).build();
+        //todo impl
+        return ResponseEntity
+                .created(URI.create("api/v1/beer/"+createdBeerDto.getId().toString()))
+                .body(createdBeerDto);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //todo impl
+        return ResponseEntity.ok(BeerDto.builder().id(beerId).beerName("Beer Updated").build());
+    }
+
+    @DeleteMapping("/{beerId}")
+
+    public ResponseEntity<Void> deleteBeer(@PathVariable("beerId") UUID beerId) {
+        //todo impl
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
