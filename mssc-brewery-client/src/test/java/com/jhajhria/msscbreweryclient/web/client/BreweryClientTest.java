5c99d7db0c56deba17e4756c1c762b4d5618e342
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-04 12:58:40 +0530

    BrweryClient GET integration test added and passing

---
+++ mssc-brewery-client/src/test/java/com/jhajhria/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.jhajhria.msscbreweryclient.web.client;
+
+import com.jhajhria.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+}
