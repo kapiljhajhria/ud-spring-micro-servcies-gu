1f7e47c79fe94196a48740e1925c0cb26ab6f91d
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-04 11:10:09 +0530

    BeerDto and CustomerDto added

---
+++ mssc-brewery-client/src/main/java/com/jhajhria/msscbreweryclient/web/model/BeerDto.java
@@ -0,0 +1,11 @@
+package com.jhajhria.msscbreweryclient.web.model;
+
+import java.util.UUID;
+
+public class BeerDto {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
