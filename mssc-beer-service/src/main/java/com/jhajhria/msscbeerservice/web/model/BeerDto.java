ef291aea348f1e9e3d7c78fdbf0f897c5ade885d
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-05 10:32:34 +0530

    Validation added to BeerDto POJO

--- mssc-beer-service/src/main/java/com/jhajhria/msscbeerservice/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/com/jhajhria/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,15 +18,34 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    //above properties marked as null so that they can only be set on the server and no client can set them
+
+
+    @NotBlank(message = "Beer name is required")
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
