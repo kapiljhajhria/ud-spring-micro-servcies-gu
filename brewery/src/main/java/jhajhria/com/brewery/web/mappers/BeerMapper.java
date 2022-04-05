613959ae6a3eaadb57a6e9579059c7dd23eb27d8
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-05 15:42:54 +0530

    using DateMapper in BeerMapper @Mapper(uses=..)

--- brewery/src/main/java/jhajhria/com/brewery/web/mappers/BeerMapper.java
+++ brewery/src/main/java/jhajhria/com/brewery/web/mappers/BeerMapper.java
@@ -5,7 +5,7 @@ import jhajhria.com.brewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = { DateMapper.class })
 public interface BeerMapper {
 
     BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
