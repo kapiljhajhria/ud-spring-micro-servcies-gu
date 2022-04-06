968b9ef85fbfefa9d804f2d780161f00a0e1c169
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-06 18:37:15 +0530

    add some java8 modules which have been removed.
    fix for error
    repo
    https://github.com/FasterXML/jackson-modules-java8
    
    com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Java 8 date/time type `java.time.OffsetDateTime` not supported by default: add Module "com.fasterxml.jackson.datatype:jackson-datatype-jsr310" to enable handling

--- jackson-example/src/main/java/com/jhajhria/jacksonexample/JacksonExampleApplication.java
+++ jackson-example/src/main/java/com/jhajhria/jacksonexample/JacksonExampleApplication.java
@@ -1,6 +1,8 @@
 package com.jhajhria.jacksonexample;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +16,9 @@ public class JacksonExampleApplication {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		return JsonMapper.builder()
+				.addModule(new JavaTimeModule())
+				.build();
 	}
 
 }
