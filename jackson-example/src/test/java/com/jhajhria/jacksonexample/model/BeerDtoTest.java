9b8658cc56c1a3a40340e491b0f682becbab17e5
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-06 18:29:16 +0530

    BeerDtoTest sterialize test

--- jackson-example/src/test/java/com/jhajhria/jacksonexample/model/BeerDtoTest.java
+++ jackson-example/src/test/java/com/jhajhria/jacksonexample/model/BeerDtoTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
-class BeerDtoTest {
-  
+package com.jhajhria.jacksonexample.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+@JsonTest
+class BeerDtoTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    BeerDtoTest() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testSerializeBeerDto() throws JsonProcessingException {
+
+        BeerDto beerDto = getBeerDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+
+    }
+
 }
