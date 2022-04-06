216c61c27c4abf066aadbbe275b0c52cc8fe9b61
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-06 16:28:37 +0530

    base/helper class created for BeerDtoTest

--- jackson-example/src/test/java/com/jhajhria/jacksonexample/model/BaseTest.java
+++ jackson-example/src/test/java/com/jhajhria/jacksonexample/model/BaseTest.java
@@ -1,2 +1,19 @@
-package com.jhajhria.jacksonexample.model;public class BaseTest {
+package com.jhajhria.jacksonexample.model;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class BaseTest {
+
+    BeerDto getBeerDto() {
+        return BeerDto.builder()
+                .beerName("Wheat beer")
+                .beerStyle("Pale ale")
+                .id(UUID.randomUUID())
+                .createdDate(OffsetDateTime.now())
+                .lastUpdatedDate(OffsetDateTime.now())
+                .upc(123456789L)
+                .build();
+
+    }
 }
