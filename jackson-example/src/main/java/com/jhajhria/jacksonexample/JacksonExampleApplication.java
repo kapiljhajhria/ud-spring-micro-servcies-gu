f6ae7d081fbb1025534dd012c84f9c3678e0e16e
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-06 18:28:28 +0530

    add objectMapper bean to be Autowired

--- jackson-example/src/main/java/com/jhajhria/jacksonexample/JacksonExampleApplication.java
+++ jackson-example/src/main/java/com/jhajhria/jacksonexample/JacksonExampleApplication.java
@@ -1,13 +1,20 @@
 package com.jhajhria.jacksonexample;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JacksonExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JacksonExampleApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
 
 }
