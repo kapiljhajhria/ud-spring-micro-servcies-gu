b580ecbc5acdfaa868db26b284d6963fc79f5a5b
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-10 21:49:06 +0530

    enable caching support in the application
    https://bit.ly/37yTFb8

--- beer-service/src/main/java/com/jhajhria/beerservice/BeerServiceApplication.java
+++ beer-service/src/main/java/com/jhajhria/beerservice/BeerServiceApplication.java
@@ -2,12 +2,33 @@ package com.jhajhria.beerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
 
 @SpringBootApplication
+@EnableCaching
 public class BeerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeerServiceApplication.class, args);
 	}
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig() //
+				.prefixCacheNameWith(this.getClass().getPackageName() + ".") //
+				.entryTtl(Duration.ofSeconds(30L)) // TODO:/// change duration
+				.disableCachingNullValues();
+
+		return RedisCacheManager.builder(connectionFactory) //
+				.cacheDefaults(config) //
+				.build();
+	}
+
 
 }
