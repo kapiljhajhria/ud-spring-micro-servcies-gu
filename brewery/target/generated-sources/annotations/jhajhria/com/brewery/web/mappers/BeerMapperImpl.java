8eb0bbd6d18f6a36a4c7ab2011221fb84cea4c08
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-17 22:43:03 +0530

    using dateMapper in BeerMapperImpl to convert date formats

--- brewery/target/generated-sources/annotations/jhajhria/com/brewery/web/mappers/BeerMapperImpl.java
+++ brewery/target/generated-sources/annotations/jhajhria/com/brewery/web/mappers/BeerMapperImpl.java
@@ -6,13 +6,19 @@ import jhajhria.com.brewery.domain.Beer.BeerBuilder;
 import jhajhria.com.brewery.web.model.BeerDto;
 import jhajhria.com.brewery.web.model.BeerDto.BeerDtoBuilder;
 import jhajhria.com.brewery.web.model.v2.BeerStyle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-04-05T14:30:04+0530",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+    date = "2022-04-14T18:02:41+0530",
+    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
+@Component
 public class BeerMapperImpl implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
 
     @Override
     public BeerDto beerToBeerDto(Beer beer) {
@@ -22,11 +28,13 @@ public class BeerMapperImpl implements BeerMapper {
 
         BeerDtoBuilder beerDto = BeerDto.builder();
 
-        beerDto.id( beer.getId() );
         beerDto.beerName( beer.getBeerName() );
         if ( beer.getBeerStyle() != null ) {
             beerDto.beerStyle( beer.getBeerStyle().name() );
         }
+        beerDto.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDto.id( beer.getId() );
+        beerDto.lastUpdatedDate( dateMapper.asOffsetDateTime( beer.getLastUpdatedDate() ) );
         beerDto.upc( beer.getUpc() );
 
         return beerDto.build();
@@ -40,11 +48,13 @@ public class BeerMapperImpl implements BeerMapper {
 
         BeerBuilder beer = Beer.builder();
 
-        beer.id( beerDto.getId() );
         beer.beerName( beerDto.getBeerName() );
         if ( beerDto.getBeerStyle() != null ) {
             beer.beerStyle( Enum.valueOf( BeerStyle.class, beerDto.getBeerStyle() ) );
         }
+        beer.createdDate( dateMapper.asTimestamp( beerDto.getCreatedDate() ) );
+        beer.id( beerDto.getId() );
+        beer.lastUpdatedDate( dateMapper.asTimestamp( beerDto.getLastUpdatedDate() ) );
         beer.upc( beerDto.getUpc() );
 
         return beer.build();
